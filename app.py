d514dda43b11cfcfc5124c07ad956efecd37c642
Brian Suda <brian@suda.co.uk>
2020-02-01 23:28:09 +0000

    Adding unsubscribe field

--- app.py
+++ app.py
@@ -19,9 +19,8 @@ from models import Subscriber
 
 @app.route("/")
 def subscribe():
-	c = {}
-	c.update({'error':None})
-	c.update({'results':None})
+	error = None
+	results = None
 	if request.method == 'POST':
 		try:
 			name=request.form.get('name',None)
@@ -35,29 +34,28 @@ def subscribe():
 				)
 				db.session.add(person)
 				db.session.commit()
-				c['results'] = '{} is now subscribed.'.format(person.email)
+				results = '{} is now subscribed.'.format(person.email)
 			else:
 				error = "Email is required."
 		except:
 			error = "Error creating subscriber."
 
-	return render_template("subscribe.html", c)
+	return render_template("subscribe.html", error=error, results=results)
 
 @app.route("/unsubscribe/<id>")
 def unsubscribe(email):
-	c = {}
-	c.update({'error':None})
-	c.update({'results':None})
+	error=None
+	results=None
 	if request.method == 'POST':
 		try:
 			person = Subscriber.query.filter_by(email=email).first()
-			c['results'] = '{} as been unsubscribed.'.format(person.email)
+			results = '{} as been unsubscribed.'.format(person.email)
 			db.session.delete(person)
 			db.session.commit()
 		except:
 			error = "Could not find that email."
 
-	return render_template("unsubscribe.html", c)
+	return render_template("unsubscribe.html", , error=error, results=results)
 
 @app.route("/confirm/<id>")
 def confirm(id):
