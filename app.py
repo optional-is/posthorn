73ec2b2fbb68645f38d35e564c558ab4f724a074
Brian Suda <brian@suda.co.uk>
2020-02-01 23:19:53 +0000

    Adding unsubscribe field

--- app.py
+++ app.py
@@ -22,8 +22,20 @@ def subscribe():
     return "Hello World!"
 
 @app.route("/unsubscribe/<id>")
-def unsubscribe(id):
-    return "name : {}".format(name)
+def unsubscribe(email):
+	c = {}
+	c.update({'error':None})
+	c.update({'results':None})
+	if request.method == 'POST':
+		try:
+			person = Subscriber.query.filter_by(email=email).first()
+			c['results'] = '{} as been unsubscribed.'.format(person.email)
+			db.session.delete(person)
+			db.session.commit()
+		except:
+			error = "Could not find that email."
+
+	return render_template("unsubscribe.html", c)
 
 @app.route("/confirm/<id>")
 def confirm(id):
